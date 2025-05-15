6c90e79a781a032e3fbeb19846349296e57b9b28
dungnguyen1 <dungnguyen@teqnological.asia>
2025-05-15 16:09:46 +0700

    Fix proto gen bug

--- proto/product/product_service.pb.go
+++ proto/product/product_service.pb.go
@@ -7,7 +7,7 @@
 package product
 
 import (
-	"github.com/meokg456/ecommerce/proto/common"
+	common "github.com/meokg456/ecommerce/proto/common"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
